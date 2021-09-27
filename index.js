7f6008624ffc506acff3c3a702f07d4968cb66e5
AurionHoward <aurionhoward@gmail.com>
2021-09-27 13:42:51 -0400

    commit tree 2d97b0ba173a9895315775a3263ec418ccbaf90b

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-const dodger = document.getElementById("dodger");
-    function moveDodgerLeft() {
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left = parseInt(leftNumbers, 10);
-  
-    if (left > 0) {
-      dodger.style.left = `${left - 1}px`;
-    }
+const dodger = document.getElementById('dodger');
+dodger.style.backgroundColor = '#FF69B4';
+dodger.style.bottom = '100px';
+dodger.style.left = '0px';
+
+document.addEventListener('keydown' function (e){
+  if(event.key === 'ArrowLeft') {
+    const leftNumbers = dodger.style.left.replace('px', '');
+    cosnt left = parseInt(leftNumbers, 10);
+    
+    dodger.style.left = `${left -1}px`;
   }
-
-
-    function moveDodgerRight() {
-      const leftNumbers = dodger.style.left.replace("px", "");
-      const left = parseInt(leftNumbers, 10);
-
-      if (left <  360) {
-          dodger.style.left =`${left +1}px`;
-      }
-  }
-
+});
